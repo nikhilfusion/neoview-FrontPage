c5cd0b413093c1650796cb68380d6269eec6bfce
Nikhil T Nair <nikhil.nair@plancess.com>
2016-12-09 00:49:41 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,37 @@
+angular.module('landingApp', [])
+.controller('landingController', ['$scope', function($scope) {
+  $scope.cameraInfo = [
+    {
+      id : 1,
+      name: "Camera1",
+      status: 0,
+      description: "Baby room camera"
+    },{
+      id : 2,
+      name: "Camera2",
+      status: 1,
+      description: "ICU room camera"
+    },{
+      id : 3,
+      name: "Camera3",
+      status: 1,
+      description: "Hallway camera"
+    },{
+      id : 4,
+      name: "Camera4",
+      status: 0,
+      description: "Incubator camera"
+    },{
+      id : 5,
+      name: "Camera5",
+      status: 1,
+      description: "Bedside camera"
+    },{
+      id : 6,
+      name: "Camera6",
+      status: 1,
+      description: "Test one"
+    }
+  ];
+  console.log("this is the controller");
+}]);
